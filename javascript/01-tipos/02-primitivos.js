99f7fc5043024cb69c58e49f43794cc0e630e4b3
jesue12 <160826009+jesue12@users.noreply.github.com>
2024-11-07 10:31:33 -0600

    Delete javascript directory

--- javascript/01-tipos/02-primitivos.js
+++
@@ -1,6 +0,0 @@
-let numero = 1;
-let texto = "hola mundo";
-let verdadero= true;
-let falso = false;
-let noDefi= undefined;
-let nulo = null;
--- javascript/01-tipos/09-argumentos.js
+++
@@ -1,9 +0,0 @@
-function suma(a, b){
-    console.log(arguments);
-return a + b + 2;
-}
-
-let resultado = suma(5, 6, 1, 2,3);
-console.log(resultado);
-console.log(typeof suma);
-
